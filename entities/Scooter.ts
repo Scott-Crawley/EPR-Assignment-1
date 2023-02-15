e6d56121c4a3af565201ed5c1438053d222f81cb
I-Knight-I <kpsc1822@gmail.com>
2023-02-15 15:28:07 +0000

    Add coordinates to Scooter

--- entities/Scooter.ts
+++ entities/Scooter.ts
@@ -7,16 +7,19 @@ export class Scooter implements Serialisable {
     lastUsed        : number;
     usedBy          : number;
     lastStationId   : number;
+    coordinates     : string;
 
-    constructor(_id: number | undefined, _available: boolean, _lastUsed: number, _usedBy: number, _lastStationId: number) {
+    constructor(_id: number | undefined, _available: boolean, _lastUsed: number, _usedBy: number, _lastStationId: number, _coordinates: string) {
         this.id             = _id;
         this.available      = _available;
         this.lastUsed       = _lastUsed;
         this.usedBy         = _usedBy;
         this.lastStationId  = _lastStationId;
+        this.coordinates    = _coordinates;
+        
     }
 
     static fromRow(row: any): Scooter {
-        return new Scooter(row.ID, row.Available, row.LastUsed, row.UsedBy, row.LastStation);
+        return new Scooter(row.ID, row.Available, row.LastUsed, row.UsedBy, row.LastStation, row.Coordinates);
     }
 }
